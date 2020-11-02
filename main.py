e3a369760feb8dc9c592aa5f84ad70f6f486d56e
bvandijkman <bvandijkman@deloitte.nl>
2020-11-02 11:39:14 +0100

    Requirements file and first streamlit line

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import streamlit as st
 
 
 def remove_password():
     pass
 
+def main():
+    remove_password()
+    st.title("Hello World")
+
+
+if __name__ == "__main__":
+    main()
